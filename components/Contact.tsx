a1c631b8b0c1d2cac83ad1c615376691ee561600
dagamadhav <115336290+dagamadhav@users.noreply.github.com>
2024-08-12 15:03:22 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,17 +7,17 @@ const Contact = () => {
       className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
     >
       <p className="font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wide">
-        04. What’s Next?
+        04. Whats Next?
       </p>
       <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[600px] text-center text-textDark">
         Thankyou for taking a look through my introductory website, i would like to work onto new oppotunities and learn more about the emerging tech.
-        Let's join our hand and provide better solution with advanced tech.
+        Lets join our hand and provide better solution with advanced tech.
       </p>
 
       <a href="mailto:dagamadhav1@gmail.com">
         <button className="w-40 h-14 border border-textGreen mt-6 font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300">
-          Let's Connect
+          Lets Connect
         </button>
       </a>
     </section>
